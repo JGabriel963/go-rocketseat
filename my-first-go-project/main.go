f479d9d342fa0c8ae4ee10dee4af4f534449ae39
João Gabriel <joaogabriel9633@gmail.com>
2026-06-26 11:59:28 -0300

    Loops

--- my-first-go-project/main.go
+++ my-first-go-project/main.go
@@ -2,22 +2,88 @@ package main
 
 import (
 	"fmt"
-	"my-first-go-project/pacote"
+	// "math"
+	// "my-first-go-project/pacote"
 )
 
 func main() {
-	fmt.Println("hello, world!")
-	pacote.GetFoo()
-	sayHello()
-	fmt.Println(sum(10, 10))
+	// Funções
+	// fmt.Println("hello, world!")
+	// pacote.GetFoo()
+	// sayHello()
+	// fmt.Println(sum(10, 10))
 
-	a, b := swap(10, 20)
-	fmt.Println(a, b)
+	// a, b := swap(10, 20)
+	// fmt.Println(a, b)
 
-	res, rem := divide(10, 3)
-	fmt.Println(res, rem)
+	// res, rem := divide(10, 3)
+	// fmt.Println(res, rem)
 
-	fmt.Println(sumMany(1, 2, 3, 4, 5))
+	// fmt.Println(sumMany(1, 2, 3, 4, 5))
+
+	// Variáveis
+	// var firstName, lastName string = "João", "Oliveira"
+	// fmt.Println(firstName, lastName)
+
+	// x := 10
+	// fmt.Println(x)
+
+	// Tipos
+	//
+	// bool
+	//
+	// int int8, int16, int32, int64
+	// uint uint8, uint16, uint32, uint64 uintptr
+	//
+	// byte
+	//
+	// rune
+	//
+	// float32, float64
+	//
+	// complex64, complex128
+	//
+	// string
+
+	// Array
+	// const x = 10
+	// arr := [x]int{5: 100, 9: 200} // [0 0 0 0 0 100 0 0 0 200]
+	// fmt.Println(arr)
+
+	// Loops
+
+	// var i int
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+	// for i < 10 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+	// arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// for i, elem := range arr {
+	// 	fmt.Println(i, elem)
+	// }
+	// for _, elem := range arr {
+	// 	fmt.Println(elem)
+	// }
+
+	// for range 10 {
+	// 	fmt.Println("*")
+	// }
+
+	// for i, elem := range arr {
+	// 	fmt.Println(&i, &elem)
+	// }
+
+	// Ifs
+	// if x := math.Sqrt(4); x < 5 { // Declaração de variável + If
+	// 	fmt.Println("x is less than 5")
+	// } else if x > 1 {
+	// 	fmt.Println("x is greater than 1")
+	// } else {
+	// 	fmt.Println("x is greater than 5")
+	// }
 }
 
 func sayHello() {
